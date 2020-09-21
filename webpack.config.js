c4548526984470dc15c75596fb807ee55ba192fb
Bri-b <bri.skinner02@gmail.com>
2020-09-21 15:51:32 -0500

    starting webpack over again

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,32 @@
+// const webpack = require('webpack');
 const path = require('path');
-const BUILD_DIR = path.resolve(__dirname, './server/build'); // where building is happening
-const APP_DIR = path.resolve(__dirname, './client');
+// const BUILD_DIR = path.resolve(__dirname, './public/build'); // where building is happening
+// const APP_DIR = path.resolve(__dirname, './client'); // where the build is coming from
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './client/index.js'),
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './public/build')
+  },
+}
+//   resolve: {
+//     extensions: ['', '.js', '.jsx'],
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test:/\.(js|jsx)$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: ["@babel/preset-react"]
+//           }
+//         }
+//       }
+//     ]
+//   }
+// }
