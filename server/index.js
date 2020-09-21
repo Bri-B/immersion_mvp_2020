00a77975af86c57472fc4db24363cc6fffde719e
Bri-b <bri.skinner02@gmail.com>
2020-09-21 14:02:45 -0500

    debugging using test data

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
+// Starting Express Server
 const express = require('express')
 const app = express()
 const port = 8080
 
+//middleware
+app.use(express.json())
+
+//connect test data
+const testData = require('../exampleData')
+
+// CRUD methods
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log(testData);
 })
 
+// listening on port
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`)
 })
