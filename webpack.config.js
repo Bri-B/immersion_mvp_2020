34bc2b8915fad910edbf625b84812016cc833e2a
Bri-b <bri.skinner02@gmail.com>
2020-09-21 16:24:55 -0500

    webpack errors gone and actively watching

--- webpack.config.js
+++ webpack.config.js
@@ -11,22 +11,23 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './public/build')
   },
+  watch: true,
+  // resolve: {
+  //   extensions: ['.js', '.jsx'],
+  // },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test:/\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  }
 }
-//   resolve: {
-//     extensions: ['', '.js', '.jsx'],
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test:/\.(js|jsx)$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ["@babel/preset-react"]
-//           }
-//         }
-//       }
-//     ]
-//   }
-// }
