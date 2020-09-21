5c79cfb019395649cccde7ffcb9be41e985e0ecd
Bri-b <bri.skinner02@gmail.com>
2020-09-21 14:42:08 -0500

    able to use static html with express static

--- server/index.js
+++ server/index.js
@@ -1,20 +1,24 @@
 // Starting Express Server
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//path
+const path = require('path');
+app.use(express.static(__dirname + './../public'));
 
 //middleware
-app.use(express.json())
+// app.use(express.json())
 
 //connect test data
-const testData = require('../exampleData')
+const testData = require('../exampleData');
 
 // CRUD methods
 app.get('/', (req, res) => {
-  console.log(testData);
+  res.send({test: false});
 })
 
 // listening on port
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
 })
