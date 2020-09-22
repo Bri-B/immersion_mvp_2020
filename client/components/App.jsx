63009e0da2f5cbc9c915621e6dfaa3e3ee883f9a
Bri-b <bri.skinner02@gmail.com>
2020-09-22 10:45:25 -0500

    generate random list finished, next make dynamic

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,31 +8,59 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      drinkRecipe: []
+      id: 0,
+      name: "",
+      category: "",
+      glass: "",
+      instructions: "",
+      thumbnail: "",
+      ingredientsList: "",
+      measurements: "",
+      updatedAt: "",
+      createdAt: ""
     };
 
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick() {
     // axois request to server
+    console.log("clicked!")
     axios({
       url: 'http://localhost:8080/button',
       method: 'get',
       // responseType: 'json'
     })
-    .then(result => console.log(result));
-    //state change to track button click
-    // this.setState(state => ({
-    //   isToggleOn: !state.isToggleOn
-    // }));
+    .then(resultObj => {
+      //state change to track button click
+      this.setState(state => ({
+        id: resultObj.data.id,
+        name: resultObj.data.name,
+        category: resultObj.data.category,
+        glass: resultObj.data.glass,
+        instructions: resultObj.data.instructions,
+        thumbnail: resultObj.data.thumbnail,
+        ingredientsList: resultObj.data.ingredientsList,
+        measurements: resultObj.data.measurements,
+      }));
+    })
+    .catch(err => console.error("click err", err))
   }
   render(){
+    let drinkRecipe = [this.state];
     return (
       <div>
         <h1>Header from APP.JSX </h1>
         <button onClick={this.handleClick}>Get Drink</button>
         <div>
-          <List></List>
+          <List 
+          name={this.state.name}
+          category={this.state.category}
+          glass={this.state.glass}
+          instructions={this.state.instructions}
+          thumbnail={this.state.thumbnail}
+          ingredientsList={this.state.ingredientsList}
+          measurements={this.state.measurements}
+          ></List>
         </div>
       </div>
     )
--- client/components/List.jsx
+++ client/components/List.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+const _ = require('lodash');
 
 class List extends React.Component {
   render() {
-    return <h1>Hello from List</h1>;
+    return (
+      <div>
+        <h1>Hello from List</h1>
+        <div>
+          {_.map(this.props, (item, index) => {
+            return <p key={index}>{item}</p>
+          })}
+        </div>
+      </div>
+    )
   }
 }
 
