11be1ffab98425c7a8b39fd72951660caf0d3a76
Bri-b <bri.skinner02@gmail.com>
2020-09-22 19:23:15 -0500

    button layout working

--- client/components/List.jsx
+++ client/components/List.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-// import Nickname from './Nickname';
+import Nickname from './Nickname.jsx';
 
 const _ = require('lodash');
 
@@ -16,7 +16,7 @@ class List extends React.Component {
     return (
       <div>
         <h1>Hello from List</h1>
-        {/* <Nickname /> */}
+        <Nickname />
         <button type="button" onClick={() => this.props.onDelete(this.props.name)}>Delete</button>
         <div>
           {_.map(this.props, (item, index) => {
--- client/components/Nickname.jsx
+++ client/components/Nickname.jsx
@@ -7,19 +7,12 @@ const _ = require('lodash');
 class Nickname extends React.Component {
   render() {
     return (
-      <div>hi</div>
+      <div>
+        <input type="text" id="lname" name="lname" />
+        <button type="submit">Update</button>
+      </div>
     );
   }
 }
 
 export default Nickname;
-
-/**
- *       <form action="/action_page.php">
-      <label for="fname">First name:</label>
-      <input type="text" id="fname" name="fname"><br><br>
-      <label for="lname">Last name:</label>
-      <input type="text" id="lname" name="lname"><br><br>
-      <input type="submit" value="Submit">
-      </form>
- */
